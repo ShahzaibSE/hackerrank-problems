3a0fc3880df6f44d5eaf6cfb265d3262eb7e9ad0
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2024-07-15 00:59:03 +0500

    Quicksort added

--- typescript-code/index.js
+++ typescript-code/index.js
@@ -1,1 +1,15 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 console.log("Hello World");
+var arr = [1, 2, 3];
+var arr_copy = __spreadArray([], arr, true);
+if (arr == arr_copy) {
+    console.log("Both lists are equal");
+}
--- typescript-code/index.ts
+++ typescript-code/index.ts
@@ -1,1 +1,8 @@
 console.log("Hello World")
+
+let arr: number[] = [1,2,3]
+let arr_copy: number[] = [...arr]
+
+if(arr === arr_copy){
+    console.log("Both lists are equal")
+}
